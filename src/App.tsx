87a0a418667722f6b04fcfd91ac6f5cd047938f4
44sisi <luo.siwei@u.nus.edu>
2021-01-30 19:20:31 +0800

    finish functionality for fixed input

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,67 @@ import { ReactComponent as Play } from "./icons/play.svg";
 import { ReactComponent as Pause } from "./icons/pause.svg";
 import { ReactComponent as Reset } from "./icons/reset.svg";
 
+let Timer: any;
+
 function App() {
   const totalRound = 5,
     totalExercise = 5,
     exerciseTime = 30,
     restTime = 60;
 
-  const [playing, setPlaying] = useState(false);
-  const [rest, setRest] = useState(false);
-  const [countdown, setCountdown] = useState(rest ? restTime : exerciseTime);
-  const [currentRound, setCurrentRound] = useState(1);
-  const [currentExercise, setCurrentExercise] = useState(1);
-
-  const roundTIme = exerciseTime * totalExercise + restTime;
+  const roundExerciseTime = exerciseTime * totalExercise;
+  const roundTIme = roundExerciseTime + restTime;
   const totalTime = roundTIme * totalRound;
-  const remaining =
-    countdown +
-    exerciseTime * (totalExercise - currentExercise) +
-    (rest ? 0 : restTime) +
-    roundTIme * (totalRound - currentRound);
 
-  function resetOnClick() {
+  const initialState = {
+    playing: false,
+    elapsedTime: 0,
+  };
+
+  const [playing, setPlaying] = useState(initialState.playing);
+  const [elapsedTime, setElapsedTime] = useState(initialState.elapsedTime);
+
+  function resetState() {
+    setPlaying(initialState.playing);
+    setElapsedTime(initialState.elapsedTime);
+  }
+
+  let remainingTime = totalTime - elapsedTime;
+  let currentRound = Math.floor(elapsedTime / roundTIme) + 1;
+  let roundElapsedTime = elapsedTime % roundTIme;
+  let rest = roundElapsedTime >= roundExerciseTime;
+  let currentExercise = rest
+    ? totalExercise
+    : Math.floor(roundElapsedTime / exerciseTime) + 1;
+  let countdown = rest
+    ? roundTIme - roundElapsedTime
+    : exerciseTime * currentExercise - roundElapsedTime;
+
+  document.querySelector("html")!.style.background = rest
+    ? "#25b174"
+    : "#222222";
+
+  function resetTimer() {
     setPlaying(false);
+    clearInterval(Timer);
+    resetState();
   }
 
   function playOnClick() {
     setPlaying(true);
-    var currentTimeLeft = countdown;
-    var countdownTimer = setInterval(function () {
-      currentTimeLeft--;
-      setCountdown(currentTimeLeft);
-      if (currentTimeLeft <= 0) clearInterval(countdownTimer);
+    let timeElapsed = elapsedTime;
+    Timer = setInterval(function () {
+      timeElapsed++;
+      setElapsedTime(timeElapsed);
+      if (timeElapsed >= totalTime) {
+        resetTimer();
+      }
     }, 1000);
   }
 
   function pauseOnClick() {
     setPlaying(false);
+    clearInterval(Timer);
   }
 
   return (
@@ -53,7 +78,7 @@ function App() {
         <div className="text-center">
           <div className="label">ELAPSED</div>
           <div className="content">
-            <Time seconds={totalTime - remaining}></Time>
+            <Time seconds={elapsedTime}></Time>
           </div>
         </div>
 
@@ -65,14 +90,14 @@ function App() {
         <div className="text-center">
           <div className="label">REMAINING</div>
           <div className="content">
-            <Time seconds={remaining}></Time>
+            <Time seconds={remainingTime}></Time>
           </div>
         </div>
       </div>
 
       <div className="flex-row control-row">
         <div className="left-button">
-          <Button onClick={resetOnClick}>
+          <Button onClick={resetTimer}>
             <Reset></Reset>
           </Button>
         </div>
